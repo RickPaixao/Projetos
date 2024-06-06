733dd3535eca034d1fa320a0aefa56e98f7ea0ab
Henrique Mota Paixao <110409895+RickPaixao@users.noreply.github.com>
2024-06-06 16:29:00 -0300

    Add files via upload

---
+++ Projeto filme/admin/js/login.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+    
+    let url = "https://back-login.vercel.app/usuarios"
+    let response = await fetch(url);
+    let userData = await response.json();
+
+    const usuario = userData.find(data => (data.nome === nome || data.email === nome) && data.senha === senha);
+
+
+        if (usuario) {
+            alert('Login bem-sucedido!');
+            window.location.href = 'http://127.0.0.1:5500/Projeto%20filme/admin/cadastro.html';
+        } else {
+            alert('Usuário ou senha incorretos.');
+        }
+    
+})
